6695d7007347cc99140be9d6d6fef6a79b98aea7
Tema <Tema>
2023-06-25 10:40:26 +0300

    Добавлена дз5_1

---
+++ Homework5_1/Program.cs
@@ -0,0 +1,25 @@
+﻿// // Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] numbers = new int[4];
+        Random random = new Random();
+
+        // Заполнение массива случайными числами
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = random.Next(100, 1000);
+            Console.Write(numbers[i] + " ");
+        }
+
+        int evenCount = 0;
+
+        // Подсчет количества четных чисел
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] % 2 == 0)
+            {
+                evenCount++;
+            }
+        }
+
+        Console.WriteLine("\nКоличество чётных чисел: " + evenCount);
